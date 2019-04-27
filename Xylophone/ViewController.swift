b6b9de15dd540172e8937ceb669adddfc86f9aa1
2rahulsk <30774426+2rak@users.noreply.github.com>
2019-04-27 12:10:49 +1000

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyNotePressed(_ sender: UIButton) {
+    
+        let url = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav")!
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+            
+        } catch let error as NSError {
+            print(error.description)
+        }
+        
+    }
+    
 }
 
